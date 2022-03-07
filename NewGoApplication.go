54896319db3d9c083a04f671ba4dad690a069466
Kristoffer Solheim <kristoffer.solheim@knowit.no>
2022-03-07 13:25:40 +0100

    Added things in main branch

--- NewGoApplication.go
+++ NewGoApplication.go
@@ -6,4 +6,6 @@ fmt.Println("This is added in KS_DevBranch")
 fmt.Println("Added line in dev branch")
 fmt.Println("Added this in main branch")
 fmt.Println("Added this new line in dev branch")
+
+fmt.Println("Things have been happening in main branch")
 }
