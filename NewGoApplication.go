6d6303baaad047370571aa9640388da32aa25216
Kristoffer Solheim <kristoffer.solheim@knowit.no>
2022-03-07 13:24:53 +0100

    Adding work to dev branch

--- NewGoApplication.go
+++ NewGoApplication.go
@@ -6,4 +6,6 @@ fmt.Println("This is added in KS_DevBranch")
 fmt.Println("Added line in dev branch")
 fmt.Println("Added this in main branch")
 fmt.Println("Added this new line in dev branch")
+
+fmt.Println("A lot of work has been done in dev branch")
 }
