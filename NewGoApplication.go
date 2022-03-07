7b93b1752905884d97a5bb80deed7fad21632d9b
Kristoffer Solheim <kristoffer.solheim@knowit.no>
2022-03-07 13:31:38 +0100

    Revert "Added new line in dev branch"
    This reverts commit ade6b38e8f3af3058fbaeea03285468950fa88c5.
    
    # Conflicts:
    #	NewGoApplication.go

--- NewGoApplication.go
+++ NewGoApplication.go
@@ -5,7 +5,6 @@ func main(){
 fmt.Println("This is added in KS_DevBranch")
 fmt.Println("Added line in dev branch")
 fmt.Println("Added this in main branch")
-fmt.Println("Added this new line in dev branch")
 
 fmt.Println("Things have been happening in main branch")
 fmt.Println("A lot of work has been done in dev branch")
